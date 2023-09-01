5e22b1176ef816b504189973d431b2f5538fd0c6
prongbang <prongbang@gmail.com>
2023-09-01 13:20:03 +0700

    Add function hex_to_bin

--- lazysodium/src/api.rs
+++ lazysodium/src/api.rs
@@ -6,25 +6,10 @@ pub const KX_SK_BYTES: usize = crypto_kx_SECRETKEYBYTES as usize;
 pub const KX_PK_HEX_BYTES: usize = KX_PK_BYTES * 2;
 pub const KX_SK_HEX_BYTES: usize = KX_SK_BYTES * 2;
 
-pub type PublicKey = [u8; KX_PK_BYTES];
-pub type SecretKey = [u8; KX_SK_BYTES];
-
 #[derive(Debug)]
 pub struct KeyPair {
     pub pk: Vec<u8>,
     pub sk: Vec<u8>,
-}
-
-impl KeyPair {
-    pub fn pk_hex(&self) -> String {
-        let pk_bytes = self.pk.to_vec();
-        return bin2hex(pk_bytes);
-    }
-
-    pub fn sk_hex(&self) -> String {
-        let sk_bytes = self.sk.to_vec();
-        return bin2hex(sk_bytes);
-    }
 }
 
 pub fn gen_keypair() -> KeyPair {
@@ -41,7 +26,7 @@ pub fn gen_keypair() -> KeyPair {
     }
 }
 
-pub fn bin2hex(data: Vec<u8>) -> String {
+pub fn bin_to_hex(data: Vec<u8>) -> String {
     let len = data.len();
     let mut hex_output: Vec<c_char> = vec![0; 2 * len + 1];
 
@@ -60,7 +45,41 @@ pub fn bin2hex(data: Vec<u8>) -> String {
     hex_string
 }
 
-fn vec_to_string(mut hex_output: Vec<i8>) -> String {
+pub fn hex_to_bin(hex: String) -> Vec<u8> {
+    // Determine the expected maximum length of the binary output
+    let max_binary_len = hex.len() / 2; // Two hexadecimal characters represent one byte
+
+    // Create a mutable vector to hold the binary data
+    let mut binary_data: Vec<c_uchar> = vec![0; max_binary_len];
+
+    // Declare variables for optional parameters
+    let ignore = std::ptr::null(); // No characters to ignore
+    let mut bin_len: usize = 0;
+    let mut hex_end: *const c_char = std::ptr::null();
+
+    // Convert the hexadecimal string to binary
+    let result = unsafe {
+        libsodium_sys::sodium_hex2bin(
+            binary_data.as_mut_ptr(),
+            max_binary_len,
+            hex.as_ptr() as *const c_char,
+            hex.len(),
+            ignore,
+            &mut bin_len as *mut usize,
+            &mut hex_end as *mut *const c_char,
+        )
+    };
+
+    // Check if the conversion was successful
+    if result == 0 {
+        // The conversion was successful, and bin_len contains the actual length of the binary data
+        binary_data.resize(bin_len, 0);
+    }
+
+    binary_data
+}
+
+fn vec_to_string(hex_output: Vec<i8>) -> String {
     unsafe { std::ffi::CStr::from_ptr(hex_output.as_ptr()) }
         .to_string_lossy()
         .into_owned()
@@ -75,21 +94,35 @@ mod tests {
         let keypair = gen_keypair();
         println!("{:?}", &keypair.pk.len());
         println!("{:?}", &keypair.sk.len());
-        println!("{:?}", &keypair.pk_hex());
-        println!("{:?}", &keypair.sk_hex());
         println!("{:?}", &keypair);
     }
 
     #[test]
-    fn test_bin2hex() {
+    fn test_bin_to_hex() {
         let result = gen_keypair();
-        let pk = result.pk.to_vec();
-        let sk = result.sk.to_vec();
-        let pk_hex = bin2hex(pk);
-        let sk_hex = bin2hex(sk);
+        let pk = result.pk;
+        let sk = result.sk;
+        let pk_hex = bin_to_hex(pk);
+        let sk_hex = bin_to_hex(sk);
         println!("{:?}", &pk_hex);
         println!("{:?}", &sk_hex);
-        println!("{:?}", &pk_hex.len());
-        println!("{:?}", &sk_hex.len());
+        assert_eq!(pk_hex.len(), KX_PK_HEX_BYTES);
+        assert_eq!(sk_hex.len(), KX_SK_HEX_BYTES);
+    }
+
+    #[test]
+    fn test_hex_to_bin() {
+        let pk_hex = "1be470dad3f6d448c2a16272e4aac228f76e64870694e8569423d7518bf74c26";
+        let sk_hex = "284151a4157da7206525358e385ac59769d1db4ebfe977c3dfc8e8f411fe3ebc";
+        let pk_bytes = hex_to_bin(pk_hex.to_string());
+        let sk_bytes = hex_to_bin(sk_hex.to_string());
+        let actual_pk_hex = bin_to_hex(pk_bytes.clone());
+        let actual_sk_hex = bin_to_hex(sk_bytes.clone());
+        println!("{:?}", &pk_bytes);
+        println!("{:?}", &sk_bytes);
+        println!("{:?}", &actual_pk_hex);
+        println!("{:?}", &actual_sk_hex);
+        assert_eq!(actual_pk_hex, pk_hex);
+        assert_eq!(actual_sk_hex, sk_hex);
     }
 }
