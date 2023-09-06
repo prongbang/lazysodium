f66c3afdd19dec0be22c3f99fa4183b97de58c31
prongbang <prongbang@gmail.com>
2023-09-07 01:26:38 +0700

    Move logic to loader

--- lib/lazysodium.dart
+++ lib/lazysodium.dart
@@ -1,7 +1,5 @@
-import 'dart:ffi';
-import 'dart:io';
-
 import 'package:lazysodium/lazysodium.binding.dart';
+import 'package:lazysodium/lazysodium.loader.dart';
 
 export 'lazysodium.binding.dart';
 export 'lazysodium_extensions.dart';
@@ -22,31 +20,4 @@ class Lazysodium extends LazysodiumBinding {
   }
 
   factory Lazysodium.instance() => Lazysodium(lazysodium);
-}
-
-final DynamicLibrary lazysodium = _load();
-
-DynamicLibrary _load() {
-  if (Platform.isAndroid) {
-    return DynamicLibrary.open('libsodium.so');
-  }
-  if (Platform.isIOS) {
-    return DynamicLibrary.process();
-  }
-  if (Platform.isMacOS) {
-    // assuming user installed libsodium as per the installation instructions
-    // see also https://libsodium.gitbook.io/doc/installation
-    return DynamicLibrary.open('/usr/local/lib/libsodium.dylib');
-  }
-  if (Platform.isLinux) {
-    // assuming user installed libsodium as per the installation instructions
-    // see also https://libsodium.gitbook.io/doc/installation
-    return DynamicLibrary.open('/usr/local/lib/libsodium.so');
-  }
-  if (Platform.isWindows) {
-    // assuming user installed libsodium as per the installation instructions
-    // see also https://py-ipv8.readthedocs.io/en/latest/preliminaries/install_libsodium/
-    return DynamicLibrary.open('C:\\Windows\\System32\\libsodium.dll');
-  }
-  throw Exception('platform not supported');
 }
---
+++ lib/lazysodium.loader.dart
@@ -0,0 +1,29 @@
+import 'dart:ffi';
+import 'dart:io';
+
+final DynamicLibrary lazysodium = _load();
+
+DynamicLibrary _load() {
+  if (Platform.isAndroid) {
+    return DynamicLibrary.open('libsodium.so');
+  }
+  if (Platform.isIOS) {
+    return DynamicLibrary.process();
+  }
+  if (Platform.isMacOS) {
+    // assuming user installed libsodium as per the installation instructions
+    // see also https://libsodium.gitbook.io/doc/installation
+    return DynamicLibrary.open('/usr/local/lib/libsodium.dylib');
+  }
+  if (Platform.isLinux) {
+    // assuming user installed libsodium as per the installation instructions
+    // see also https://libsodium.gitbook.io/doc/installation
+    return DynamicLibrary.open('/usr/local/lib/libsodium.so');
+  }
+  if (Platform.isWindows) {
+    // assuming user installed libsodium as per the installation instructions
+    // see also https://py-ipv8.readthedocs.io/en/latest/preliminaries/install_libsodium/
+    return DynamicLibrary.open('C:\\Windows\\System32\\libsodium.dll');
+  }
+  throw Exception('platform not supported');
+}
